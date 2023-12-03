906a91e7318b3dd6aaf0ec8cf511771dd4ad61cd
Marcell Nemes <nemes@sch.bme.hu>
2023-12-03 20:38:12 +0000

    Add part parsing

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict
 from typing import Iterable
 
@@ -5,6 +6,16 @@ from typing import Iterable
 class Engine:
     def __init__(self, data: Iterable[str]) -> None:
         self.schematic = defaultdict(lambda: '.')
+        self.parts = dict()
         for y, line in enumerate(map(str.strip, data)):
+            parsed_number = False
             for x, c in enumerate(line):
                 self.schematic[(y, x)] = line[x]
+                if c.isnumeric():
+                    if not parsed_number:
+                        parsed_number = True
+                        self.parts[(y, x)] = int(
+                            re.match(r'(\d+).*', line[x:]).group(1)
+                        )
+                else:
+                    parsed_number = False
--- day3/test.py
+++ day3/test.py
@@ -14,13 +14,29 @@ TEST_INPUT = [
     '.664.598..'
 ]
 
+TEST_PARTS = {
+    (0, 0): 467, (0, 5): 114,
+    (2, 2): 35, (2, 6): 633,
+    (4, 0): 617,
+    (5, 7): 58,
+    (6, 2): 592,
+    (7, 6): 755,
+    (9, 1): 664, (9, 5): 598
+}
+
 
 class TestParser(unittest.TestCase):
-    def test_parser(self):
-        engine = Engine(TEST_INPUT)
+    def setUp(self):
+        self.engine = Engine(TEST_INPUT)
+
+    def test_parse_schematics(self):
         for y in range(len(TEST_INPUT)):
             for x in range(len(TEST_INPUT[0])):
-                self.assertEqual(engine.schematic[(y, x)], TEST_INPUT[y][x])
+                self.assertEqual(self.engine.schematic[(y, x)],
+                                 TEST_INPUT[y][x])
+
+    def test_parse_parts(self):
+        self.assertDictEqual(self.engine.parts, TEST_PARTS)
 
 
 if __name__ == '__main__':
