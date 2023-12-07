3a615c5b2680714829718c85afeebb816466f39e
Nemes Marcell <nemes@sch.bme.hu>
2023-12-07 20:30:54 +0000

    Day5 parse - green

--- day5/day5.py
+++ day5/day5.py
@@ -3,5 +3,14 @@ from typing import TextIO
 
 class Almanac:
     def __init__(self, data: TextIO) -> None:
-        self.seeds = []
+        seeds, *maps = ''.join(data.readlines()).split('\n\n')
+        self.seeds = [int(seed) for seed in seeds.split(':')[1].split()]
         self.map_ranges = []
+        for map_ in maps:
+            rules = []
+            for line in map_.split('\n')[1:]:
+                if line:
+                    dest, source, length = map(int, line.split())
+                    rules.append((range(source, source + length),
+                                  range(dest, dest + length)))
+            self.map_ranges.append(rules)
