1d67fc92ca52fd2a4a1e4da50f15f00eac7f8362
Nemes Marcell <nemes@sch.bme.hu>
2023-12-08 21:06:13 +0000

    Day7 consolidate buckets

--- day7/day7.py
+++ day7/day7.py
@@ -13,38 +13,19 @@ class CamelCards:
             self.bets.append(int(bet))
 
     @staticmethod
-    def bucket(hand: str) -> str:
-        match (cards := Counter(hand)).most_common(1)[0][1]:
-            case 1:
-                return 'high_card'
-            case 2:
-                pairs = len([p for p in cards.values() if p == 2])
-                return 'one_pair' if pairs == 1 else 'two_pairs'
-            case 3:
-                return 'full_house' if 2 in cards.values() \
-                    else 'three_of_a_kind'
-            case 4:
-                return 'four_of_a_kind'
-            case 5:
-                return 'five_of_a_kind'
-
-    def buckets(self) -> dict[str, list[tuple[str, int]]]:
-        buckets = defaultdict(list)
-        for hand, bet in zip(self.hands, self.bets):
-            buckets[self.bucket(hand)].append((hand, bet))
-        return buckets
-
-    @staticmethod
     def get_key(mapped: str) -> Callable[[tuple[str, int]], str]:
         return lambda item: item[0].translate(str.maketrans('TJQKA', mapped))
 
+    @staticmethod
+    def prep(hand: str) -> tuple[Counter[str], int]:
+        return Counter(hand), 0
+
     def part_one(self) -> int:
-        return self.winnings(self.buckets, self.get_key('ABCDE'))
+        return self.winnings(self.prep, self.get_key('ABCDE'))
 
     @staticmethod
-    def joker_bucket(hand: str) -> str:
-        cards = Counter(hand)
-        jokers = cards.pop('J', 0)
+    def bucket(hand: str, prep_fun: Callable) -> str:
+        cards, jokers = prep_fun(hand)
         if not cards:
             return 'five_of_a_kind'
         match cards.most_common(1)[0][1]:
@@ -65,17 +46,19 @@ class CamelCards:
             case 5:
                 return 'five_of_a_kind'
 
-    def joker_buckets(self) -> dict[str, list[tuple[str, int]]]:
-        buckets = defaultdict(list)
-        for hand, bet in zip(self.hands, self.bets):
-            buckets[self.joker_bucket(hand)].append((hand, bet))
-        return buckets
+    @staticmethod
+    def joker_prep(hand: str) -> tuple[Counter[str], int]:
+        cards = Counter(hand)
+        jokers = cards.pop('J', 0)
+        return cards, jokers
 
     def part_two(self) -> int:
-        return self.winnings(self.joker_buckets, self.get_key('A0CDE'))
+        return self.winnings(self.joker_prep, self.get_key('A0CDE'))
 
-    def winnings(self, bucket_fun: Callable, key_fun: Callable) -> int:
-        buckets = bucket_fun()
+    def winnings(self, prep_fun: Callable, key_fun: Callable) -> int:
+        buckets = defaultdict(list)
+        for hand, bet in zip(self.hands, self.bets):
+            buckets[self.bucket(hand, prep_fun)].append((hand, bet))
         winnings = 0
         for rank, (hand, bet) in enumerate(chain(
                 sorted(buckets['high_card'], key=key_fun),
--- day7/test.py
+++ day7/test.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import defaultdict
 from day7 import CamelCards
 
 TEST_DATA = [
@@ -21,6 +22,35 @@ JOKER_BUCKETS = {
     'four_of_a_kind': [('T55J5', 684), ('KTJJT', 220), ('QQQJA', 483)]
 }
 
+MAPPINGS = [
+    ('23456', 'high_card'),
+    ('789T7', 'one_pair'),
+    ('JJQKK', 'two_pairs'),
+    ('AAA23', 'three_of_a_kind'),
+    ('55A55', 'four_of_a_kind'),
+    ('KKKKK', 'five_of_a_kind')
+]
+
+JOKER_MAPPINGS = [
+    ('23456', 'high_card'),
+    ('2345J', 'one_pair'),
+    ('234JJ', 'three_of_a_kind'),
+    ('23JJJ', 'four_of_a_kind'),
+    ('2JJJJ', 'five_of_a_kind'),
+    ('JJJJJ', 'five_of_a_kind'),
+    ('22345', 'one_pair'),
+    ('2234J', 'three_of_a_kind'),
+    ('223JJ', 'four_of_a_kind'),
+    ('22JJJ', 'five_of_a_kind'),
+    ('22334', 'two_pairs'),
+    ('2233J', 'full_house'),
+    ('22234', 'three_of_a_kind'),
+    ('2223J', 'four_of_a_kind'),
+    ('222JJ', 'five_of_a_kind'),
+    ('22223', 'four_of_a_kind'),
+    ('2222J', 'five_of_a_kind'),
+    ('22222', 'five_of_a_kind')
+]
 
 class TestCards(unittest.TestCase):
     @classmethod
@@ -34,41 +64,31 @@ class TestCards(unittest.TestCase):
         self.assertListEqual(self.cards.bets, bets)
 
     def testBucket(self):
-        self.assertEqual(self.cards.bucket('23456'), 'high_card')
-        self.assertEqual(self.cards.bucket('789T7'), 'one_pair')
-        self.assertEqual(self.cards.bucket('JJQKK'), 'two_pairs')
-        self.assertEqual(self.cards.bucket('AAA23'), 'three_of_a_kind')
-        self.assertEqual(self.cards.bucket('55A55'), 'four_of_a_kind')
-        self.assertEqual(self.cards.bucket('KKKKK'), 'five_of_a_kind')
+        for hand, bucket in MAPPINGS:
+            self.assertEqual(self.cards.bucket(hand, self.cards.prep),
+                             bucket)
 
     def testBuckets(self):
-        self.assertDictEqual(self.cards.buckets(), BUCKETS)
+        buckets = defaultdict(list)
+        for hand, bet in map(str.split, TEST_DATA):
+            buckets[self.cards.bucket(hand, self.cards.prep)] \
+                .append((hand, int(bet)))
+        self.assertDictEqual(buckets, BUCKETS)
 
     def testPart1(self):
         self.assertEqual(self.cards.part_one(), 6440)
 
     def testJokerBucket(self):
-        self.assertEqual(self.cards.joker_bucket('23456'), 'high_card')
-        self.assertEqual(self.cards.joker_bucket('2345J'), 'one_pair')
-        self.assertEqual(self.cards.joker_bucket('234JJ'), 'three_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('23JJJ'), 'four_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('2JJJJ'), 'five_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('JJJJJ'), 'five_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('22345'), 'one_pair')
-        self.assertEqual(self.cards.joker_bucket('2234J'), 'three_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('223JJ'), 'four_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('22JJJ'), 'five_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('22334'), 'two_pairs')
-        self.assertEqual(self.cards.joker_bucket('2233J'), 'full_house')
-        self.assertEqual(self.cards.joker_bucket('22234'), 'three_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('2223J'), 'four_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('222JJ'), 'five_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('22223'), 'four_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('2222J'), 'five_of_a_kind')
-        self.assertEqual(self.cards.joker_bucket('22222'), 'five_of_a_kind')
+        for hand, bucket in JOKER_MAPPINGS:
+            self.assertEqual(self.cards.bucket(hand, self.cards.joker_prep),
+                             bucket)
 
     def testJokerBuckets(self):
-        self.assertDictEqual(self.cards.joker_buckets(), JOKER_BUCKETS)
+        buckets = defaultdict(list)
+        for hand, bet in map(str.split, TEST_DATA):
+            buckets[self.cards.bucket(hand, self.cards.joker_prep)] \
+                .append((hand, int(bet)))
+        self.assertDictEqual(buckets, JOKER_BUCKETS)
 
     def testPart2(self):
         self.assertEqual(self.cards.part_two(), 5905)
