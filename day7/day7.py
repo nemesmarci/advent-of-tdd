e9f8fbdf36323837b15f51a1a3a4c9c7e18d5578
Nemes Marcell <nemes@sch.bme.hu>
2023-12-08 20:37:43 +0000

    Day7 part2 - green

--- day7/day7.py
+++ day7/day7.py
@@ -52,5 +52,50 @@ class CamelCards:
             winnings += (rank + 1) * bet
         return winnings
 
+    @staticmethod
+    def joker_bucket(hand: str) -> str:
+        cards = Counter(hand)
+        jokers = cards.pop('J', 0)
+        if not cards:
+            return 'five_of_a_kind'
+        match cards.most_common(1)[0][1]:
+            case 1:
+                return ('high_card', 'one_pair', 'three_of_a_kind',
+                        'four_of_a_kind', 'five_of_a_kind')[jokers]
+            case 2:
+                pairs = len([p for p in cards.values() if p == 2])
+                return ('one_pair', 'three_of_a_kind', 'four_of_a_kind',
+                        'five_of_a_kind')[jokers] if pairs == 1 \
+                    else ('two_pairs', 'full_house')[jokers]
+            case 3:
+                return 'full_house' if 2 in cards.values() \
+                    else ('three_of_a_kind', 'four_of_a_kind',
+                          'five_of_a_kind')[jokers]
+            case 4:
+                return ('four_of_a_kind', 'five_of_a_kind')[jokers]
+            case 5:
+                return 'five_of_a_kind'
+
+    def joker_buckets(self) -> dict[str, list[tuple[str, int]]]:
+        buckets = defaultdict(list)
+        for hand, bet in zip(self.hands, self.bets):
+            buckets[self.joker_bucket(hand)].append((hand, bet))
+        return buckets
+
+    @staticmethod
+    def joker_key(item: tuple[str, int]) -> str:
+        return item[0].translate(str.maketrans('TJQKA', 'A0CDE'))
+
     def part_two(self) -> int:
-        return 0
+        buckets = self.joker_buckets()
+        winnings = 0
+        for rank, (hand, bet) in enumerate(chain(
+                sorted(buckets['high_card'], key=self.joker_key),
+                sorted(buckets['one_pair'], key=self.joker_key),
+                sorted(buckets['two_pairs'], key=self.joker_key),
+                sorted(buckets['three_of_a_kind'], key=self.joker_key),
+                sorted(buckets['full_house'], key=self.joker_key),
+                sorted(buckets['four_of_a_kind'], key=self.joker_key),
+                sorted(buckets['five_of_a_kind'], key=self.joker_key))):
+            winnings += (rank + 1) * bet
+        return winnings
--- day7/test.py
+++ day7/test.py
@@ -15,6 +15,12 @@ BUCKETS = {
     'three_of_a_kind': [('T55J5', 684), ('QQQJA', 483)]
 }
 
+JOKER_BUCKETS = {
+    'one_pair': [('32T3K', 765)],
+    'two_pairs': [('KK677', 28)],
+    'four_of_a_kind': [('T55J5', 684), ('KTJJT', 220), ('QQQJA', 483)]
+}
+
 
 class TestCards(unittest.TestCase):
     @classmethod
@@ -40,6 +46,29 @@ class TestCards(unittest.TestCase):
 
     def testPart1(self):
         self.assertEqual(self.cards.part_one(), 6440)
+
+    def testJokerBucket(self):
+        self.assertEqual(self.cards.joker_bucket('23456'), 'high_card')
+        self.assertEqual(self.cards.joker_bucket('2345J'), 'one_pair')
+        self.assertEqual(self.cards.joker_bucket('234JJ'), 'three_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('23JJJ'), 'four_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('2JJJJ'), 'five_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('JJJJJ'), 'five_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('22345'), 'one_pair')
+        self.assertEqual(self.cards.joker_bucket('2234J'), 'three_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('223JJ'), 'four_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('22JJJ'), 'five_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('22334'), 'two_pairs')
+        self.assertEqual(self.cards.joker_bucket('2233J'), 'full_house')
+        self.assertEqual(self.cards.joker_bucket('22234'), 'three_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('2223J'), 'four_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('222JJ'), 'five_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('22223'), 'four_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('2222J'), 'five_of_a_kind')
+        self.assertEqual(self.cards.joker_bucket('22222'), 'five_of_a_kind')
+
+    def testJokerBuckets(self):
+        self.assertDictEqual(self.cards.joker_buckets(), JOKER_BUCKETS)
 
     def testPart2(self):
         self.assertEqual(self.cards.part_two(), 5905)
