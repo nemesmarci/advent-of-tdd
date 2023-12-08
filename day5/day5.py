ab23e0c4dcd4a06aa571a487af6f75b9fdecf82d
Nemes Marcell <nemes@sch.bme.hu>
2023-12-08 14:40:18 +0000

    Day5 transform_ranges - green

--- day5/day5.py
+++ day5/day5.py
@@ -44,5 +44,25 @@ class Almanac:
                 for start, length in zip(self.seeds[0::2], self.seeds[1::2])]
 
     @staticmethod
-    def transform_ranges(original_ranges: list[range], rules):
-        return []
+    def transform_ranges(original_ranges: list[range],
+                         rules: list[tuple[range, range]]) -> list[range]:
+        new_ranges = []
+        for source_range in original_ranges:
+            if source_range.start >= rules[-1][0].stop:
+                new_ranges.append(source_range)
+                continue
+            first = next(i for i, rule in enumerate(rules)
+                         if source_range.start in rule[0])
+            for i in range(first, len(rules)):
+                new_start = (rules[i][1].start +
+                             max(source_range.start, rules[i][0].start) -
+                             rules[i][0].start)
+                if source_range.stop in rules[i][0]:
+                    new_ranges.append(range(new_start,
+                                            rules[i][1].start +
+                                            source_range.stop -
+                                            rules[i][0].start))
+                    break
+                else:
+                    new_ranges.append(range(new_start, rules[i][1].stop))
+        return [r for r in new_ranges if r]
