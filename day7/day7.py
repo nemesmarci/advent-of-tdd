e0cde9e6b40f9a3d530e181562351d53fb40d565
Nemes Marcell <nemes@sch.bme.hu>
2023-12-08 16:30:33 +0000

    Day7 parse - green

--- day7/day7.py
+++ day7/day7.py
@@ -5,3 +5,7 @@ class CamelCards:
     def __init__(self, data: Iterable[str]) -> None:
         self.hands = []
         self.bets = []
+        for line in data:
+            hand, bet = line.split()
+            self.hands.append(hand)
+            self.bets.append(int(bet))
