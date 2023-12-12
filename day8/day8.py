ab4eda15824a8abfc1bbfbfba7e8adf4c91a0843
Nemes Marcell <nemes@sch.bme.hu>
2023-12-12 14:09:37 +0000

    Day8 parse - green

--- day8/day8.py
+++ day8/day8.py
@@ -3,5 +3,11 @@ from typing import Sequence
 
 class Map:
     def __init__(self, lines: Sequence[str]) -> None:
-        self.instructions: str = ''
+        self.instructions: str = lines[0].strip()
         self.nodes: dict[str, tuple[str, str]] = {}
+        for line in lines[2:]:
+            starting, left_right = line.strip().split(' = ')
+            left, right = left_right.split(', ')
+            left = left.strip('(')
+            right = right.strip(')')
+            self.nodes[starting] = (left, right)
