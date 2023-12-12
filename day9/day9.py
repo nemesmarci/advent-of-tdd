4dbfc7b036be0607c2149019909fba2e143480f8
Nemes Marcell <nemes@sch.bme.hu>
2023-12-12 16:09:01 +0000

    Day9 refactor predict to take index of predicted value

--- day9/day9.py
+++ day9/day9.py
@@ -11,15 +11,16 @@ class Oasis:
         return [sequence[i] - sequence[i - 1] for i in range(1, len(sequence))]
 
     @staticmethod
-    def predict(sequence: Sequence[int]) -> Sequence[int]:
+    def predict(sequence: Sequence[int], index: int) -> Sequence[int]:
         predictions = []
         while not all(x == 0 for x in sequence):
-            predictions.append(sequence[-1])
+            predictions.append(sequence[index])
             sequence = Oasis.new_sequence(sequence)
         return predictions
 
     def part_one(self) -> int:
-        return sum(sum(self.predict(sequence)) for sequence in self.sequences)
+        return sum(sum(self.predict(sequence, -1))
+                   for sequence in self.sequences)
 
     def part_two(self) -> int:
         return 0
