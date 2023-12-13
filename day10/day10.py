9323e4178be71cbf6eb8911d924dd2a99d72da8d
Nemes Marcell <nemes@sch.bme.hu>
2023-12-13 20:02:40 +0000

    Day10 parse - green

--- day10/day10.py
+++ day10/day10.py
@@ -3,5 +3,9 @@ from typing import Iterable
 
 class Maze:
     def __init__(self, data: Iterable[str]) -> None:
-        self.area: dict[tuple[int, int]] = {}
-        self.start: tuple[int, int] = 0, 0
+        self.area: dict[tuple[int, int], str] = {}
+        for y, line in enumerate(map(str.strip, data)):
+            for x, c in enumerate(line):
+                if c == 'S':
+                    self.start: tuple[int, int] = y, x
+                self.area[(y, x)] = c
