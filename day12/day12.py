bc55e0df092318a1a38e6c2e0d1f0b5205584f4d
Nemes Marcell <nemes@sch.bme.hu>
2023-12-14 16:32:41 +0000

    Day12 parse - green

--- day12/day12.py
+++ day12/day12.py
@@ -5,3 +5,7 @@ class Springs:
     def __init__(self, data: Iterable[str]) -> None:
         self.rows: list[str] = []
         self.descriptions: list[list[int]] = []
+        for line in data:
+            t, d = line.strip().split()
+            self.rows.append(t)
+            self.descriptions.append(list(map(int, d.split(','))))
