9953a881f5ac6ec815cf89e7f544224d67063f58
Nemes Marcell <nemes@sch.bme.hu>
2023-12-14 12:14:39 +0000

    Day10 next_in_loop - green

--- day10/day10.py
+++ day10/day10.py
@@ -11,7 +11,20 @@ class Maze:
                 self.area[(y, x)] = c
 
     def next_in_loop(self, node: tuple[int, int], direction: str) -> tuple[tuple[int, int], str]:
-        return (0, 0), 'N'
+        if direction == 'N':
+            next_node = (node[0] - 1, node[1])
+            dirs = {'|': 'N', '7': 'W', 'F': 'E'}
+        elif direction == 'E':
+            next_node = (node[0], node[1] + 1)
+            dirs = {'-': 'E', 'J': 'N', '7': 'S'}
+        elif direction == 'S':
+            next_node = (node[0] + 1, node[1])
+            dirs = {'|': 'S', 'L': 'E', 'J': 'W'}
+        else:
+            next_node = (node[0], node[1] - 1)
+            dirs = {'-': 'W', 'L': 'N', 'F': 'S'}
+        return next_node, dirs[self.area[next_node]]
+
 
     def loop(self) -> set[tuple[int, int]]:
         return set()
