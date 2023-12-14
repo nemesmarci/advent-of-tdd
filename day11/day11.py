09fe4b2064dbf28f3b3571684786f52fb48717c4
Nemes Marcell <nemes@sch.bme.hu>
2023-12-14 15:34:20 +0000

    Day11 parse - green

--- day11/day11.py
+++ day11/day11.py
@@ -5,5 +5,10 @@ class Image:
     def __init__(self, data: Iterable[str]) -> None:
         self.area: dict[tuple[int, int], str] = {}
         self.galaxies: set[tuple[int, int]] = set()
-        self.max_y: int = 0
-        self.max_x: int = 0
+        for y, line in enumerate(data):
+            for x, c in enumerate(line.strip()):
+                self.area[(y, x)] = c
+                if c == '#':
+                    self.galaxies.add((y, x))
+        self.max_y: int = y + 1
+        self.max_x: int = x + 1
