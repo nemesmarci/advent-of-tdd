24809d662962ce2f066051b63100a64c77350625
Nemes Marcell <nemes@sch.bme.hu>
2023-12-18 22:02:53 +0000

    Day15 part2 - green

--- day15/day15.py
+++ day15/day15.py
@@ -15,10 +15,23 @@ class Boxes:
         return sum(self.hash(string) for string in self.strings)
 
     def part_two(self) -> int:
-        return 0
+        boxes = [dict() for _ in range(256)]
+        for string in self.strings:
+            match string.split('='):
+                case label, focal:
+                    box = self.hash(label)
+                    boxes[box][label] = int(focal)
+                case _:
+                    label = string.rstrip('-')
+                    box = self.hash(label)
+                    boxes[box].pop(label, None)
+        return sum((i + 1) * (j + 1) * focal
+                   for i, box in enumerate(boxes)
+                   for j, focal in enumerate(box.values()))
 
 
 if __name__ == '__main__':
     with open('input.txt') as data:
         boxes = Boxes(data.readline())
         print(boxes.part_one())
+        print(boxes.part_two())
