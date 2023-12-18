59d148aa03c2c3f1b745c0b3c9cd3205138caae6
Nemes Marcell <nemes@sch.bme.hu>
2023-12-18 21:48:20 +0000

    Day15 hash - red

--- day15/day15.py
+++ day15/day15.py
@@ -1,3 +1,7 @@
 class Boxes:
     def __init__(self, data: str) -> None:
         self.strings: list[str] = data.strip().split(',')
+
+    @staticmethod
+    def hash(string) -> int:
+        return 0
--- day15/test.py
+++ day15/test.py
@@ -3,6 +3,7 @@ from day15 import Boxes
 
 TEST_DATA = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
 STRINGS = ['rn=1', 'cm-', 'qp=3', 'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5','pc-', 'pc=6', 'ot=7']
+HASHES = [30, 253, 97, 47, 14, 180, 9, 197, 48, 214, 231]
 
 
 class TestBoxes(unittest.TestCase):
@@ -12,6 +13,10 @@ class TestBoxes(unittest.TestCase):
 
     def testParse(self):
         self.assertListEqual(self.boxes.strings, STRINGS)
+
+    def testHash(self):
+        for string, value in zip(STRINGS, HASHES):
+            self.assertEqual(self.boxes.hash(string), value)
 
 
 if __name__ == '__main__':
