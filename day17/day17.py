5e5046dfb04fc3adcae53f812285c65d621717fa
Nemes Marcell <nemes@sch.bme.hu>
2023-12-21 21:31:04 +0000

    Day17 possible_steps - green

--- day17/day17.py
+++ day17/day17.py
@@ -11,7 +11,24 @@ class Blocks:
         self.x: int = x
 
     def possible_steps(self, y: int, x: int, direction: str, straight: int) -> set[tuple[int, int, str, int]]:
-        return set()
+        possible = set()
+        if straight < 3:
+            match direction:
+                case 'E':
+                    possible.add((y, x + 1, direction, straight + 1))
+                case 'W':
+                    possible.add((y, x - 1, direction, straight + 1))
+                case 'S':
+                    possible.add((y + 1, x, direction, straight + 1))
+                case 'N':
+                    possible.add((y - 1, x, direction, straight + 1))
+        if direction in 'EW':
+            possible.add((y - 1, x, 'N', 1))
+            possible.add((y + 1, x, 'S', 1))
+        else:
+            possible.add((y, x - 1, 'W', 1))
+            possible.add((y, x + 1, 'E', 1))
+        return possible
 
     def shortest_path(self) -> int:
         return 0
