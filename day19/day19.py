174fd8915e1cbbab7dff72c57e942fc6a22a07f4
Nemes Marcell <nemes@sch.bme.hu>
2023-12-22 13:57:11 +0000

    Day19 part2 - green

--- day19/day19.py
+++ day19/day19.py
@@ -1,5 +1,7 @@
 import re
 from typing import Iterable
+from collections import deque
+from math import prod
 
 
 class Sorter:
@@ -36,10 +38,46 @@ class Sorter:
         return xmas
 
     def part_two(self) -> int:
-        return 0
+        queue = deque([('in', [])])
+        accepted = []
+        while queue:
+            step, conditions = queue.popleft()
+            if step == 'A':
+                accepted.append(conditions)
+                continue
+            if step == 'R':
+                continue
+            for i, rule in enumerate(self.workflows[step]):
+                if failed := self.workflows[step][:i]:
+                    for f in failed:
+                        match self.rule_regex.match(f).groups():
+                            case field, '<', value, _:
+                                value = int(value)
+                                conditions.append(f'{field}>{value - 1}')
+                            case field, '>', value, _:
+                                value = int(value)
+                                conditions.append(f'{field}<{value + 1}')
+                match self.rule_regex.match(rule).groups():
+                    case field, ('<' | '>') as op, value, str(next_step):
+                        queue.append((next_step, conditions + [f'{field}{op}{value}']))
+                    case next_step, *_:
+                        queue.append((next_step, conditions))
+
+        xmas = 0
+        for rules in accepted:
+            bounds = {b: range(1, 4001) for b in 'xmas'}
+            for rule in rules:
+                match self.rule_regex.match(rule).groups():
+                    case field, '<', value, _:
+                        bounds[field] = range(bounds[field].start, min(int(value), bounds[field].stop))
+                    case field, '>', value, _:
+                        bounds[field] = range(max(int(value) + 1, bounds[field].start), bounds[field].stop)
+            xmas += prod(len(b) for b in bounds.values())
+        return xmas
 
 
 if __name__ == '__main__':
     with open('input.txt') as data:
         sorter = Sorter(data)
         print(sorter.part_one())
+        print(sorter.part_two())
