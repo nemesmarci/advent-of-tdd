d6d0f77fe6628038f4bc25c32f66ffbde46619fe
Nemes Marcell <nemes@sch.bme.hu>
2023-12-22 13:12:38 +0000

    Day19 parse - green

--- day19/day19.py
+++ day19/day19.py
@@ -5,3 +5,10 @@ class Sorter:
     def __init__(self, data: Iterable[str]) -> None:
         self.workflows: dict[str, list[str]] = {}
         self.parts: list[dict[str, int]] = []
+        raw_workflows, raw_parts = ''.join(data).split('\n\n')
+        for workflow in raw_workflows.split('\n'):
+            name, steps = workflow.replace('}', '').split('{')
+            self.workflows[name] = steps.split(',')
+        x, m, a, s = 'x', 'm', 'a', 's'
+        for part in raw_parts[:-1].split('\n'):
+            self.parts.append(eval(part.replace('=', ':')))
