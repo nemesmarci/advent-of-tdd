942f4628dcbeb3b382f6c5e6b4220790d5884161
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 16:07:57 +0000

    Day21 part2

--- day21/day21.py
+++ day21/day21.py
@@ -1,5 +1,6 @@
 from typing import Iterable
 from heapq import heappush, heappop
+from numpy.polynomial.polynomial import Polynomial
 
 
 class Garden:
@@ -19,14 +20,14 @@ class Garden:
     def around(y, x):
         return (y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)
 
-    def reachable(self, steps, infinite) -> int:
+    def reachable(self, steps, infinite, even) -> int:
         visited = set()
         heap = [(0, self.start, (0, 0))]
         while heap:
             current_steps, current_plot, map_tile = heappop(heap)
             if current_steps > steps or (current_plot, map_tile) in visited:
                 continue
-            if current_steps % 2 == 0:
+            if current_steps % 2 != even:
                 visited.add((current_plot, map_tile))
             for neighbour in self.around(*current_plot):
                 (y, x), (map_y, map_x) = neighbour, map_tile
@@ -47,8 +48,15 @@ class Garden:
                     heappush(heap, (current_steps + 1, (y, x), (map_y, map_x)))
         return len(visited)
 
+    def part_two(self):
+        sequence = []
+        for i in range(3):
+            sequence.append(self.reachable(i * (self.y + 1) + self.start[0], True, i % 2))
+        return int(Polynomial.fit([0, 1, 2], sequence, 2)(26501365 // (self.y + 1)))
+
 
 if __name__ == '__main__':
     with open('input.txt') as data:
         garden = Garden(data)
-    print(garden.reachable(64, False))
+    print(garden.reachable(64, False, True))
+    print(garden.part_two())
