4c24550f1a8589866336db3d660581f934e1b1bc
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 21:30:06 +0000

    Day23 part1 - green

--- day23/day23.py
+++ day23/day23.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+from heapq import heappush, heappop
 
 
 class Walk:
@@ -11,5 +12,34 @@ class Walk:
         self.y: int = y
         self.x: int = x
 
+    def neighbours(self, y, x):
+        return [n for n in ((y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)) if n in self.area]
+
     def part_one(self) -> int:
-        return 0
+        start = (0, 1)
+        end = (self.y, self.x - 1)
+        distances = {}
+        max_hike = 0
+        heap = [(0, start, {start})]
+        while heap:
+            cur_d, (y, x), visited = heappop(heap)
+            if (y, x) in distances and distances[(y, x)] <= cur_d:
+                continue
+            distances[(y, x)] = cur_d
+            if (y, x) == end and len(visited) - 1 > max_hike:
+                max_hike = len(visited) - 1
+                continue
+            if (slope := self.area[(y, x)]) in '^<>v':
+                down = ((y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x))['^<>v'.index(slope)]
+                if down not in visited:
+                    visited.add(down)
+                    heappush(heap, (cur_d - 1, down, visited))
+            else:
+                for n in (n for n in self.neighbours(y, x) if n not in visited):
+                    heappush(heap, (cur_d - 1, n, visited | {n}))
+        return max_hike
+
+if __name__ == "__main__":
+    with open('input.txt') as data:
+        walk = Walk(data)
+    print(walk.part_one())
