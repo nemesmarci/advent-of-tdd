25cb8c43dcff57078f41cff96c9890ecba5d6329
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 22:09:30 +0000

    Day25 parse - green

--- day25/day25.py
+++ day25/day25.py
@@ -5,3 +5,7 @@ from typing import Iterable
 class Components:
     def __init__(self, data: Iterable[str]) -> None:
         self.graph: networkx.Graph = networkx.Graph()
+        for line in data:
+            component, others = line.split(': ')
+            for other in others.split():
+                self.graph.add_edge(component, other)
