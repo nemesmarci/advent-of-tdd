4fd03be18306c5a64a23255d9c36faad042238cc
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 16:45:56 +0000

    Day22 fall - red

--- day22/day22.py
+++ day22/day22.py
@@ -1,23 +1,31 @@
+from __future__ import annotations
 from typing import Iterable
 
 
 class Brick:
-    def __init__(self, x1, y1, z1, x2, y2, z2):
+    def __init__(self, id_: int, x1: int, y1: int, z1: int, x2: int, y2: int, z2: int) -> None:
+        self.id = id_
         self.x1, self.y1, self.z1 = x1, y1, z1
         self.x2, self.y2, self.z2 = x2, y2, z2
         self.supporting: set[Brick] = set()
         self.supported_by: set[Brick] = set()
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Brick) -> bool:
         return self.x1 == other.x1 and self.y1 == other.y1 and self.z1 == other.z1 and \
             self.x2 == other.x2 and self.y2 == other.y2 and self.z2 == other.z2
+
+    def __hash__(self):
+        return self.id
 
 
 class Bricks:
     def __init__(self, data: Iterable[str]) -> None:
         self.bricks: list[Brick] = []
-        for line in data:
+        for i, line in enumerate(data):
             c1, c2 = line.strip().split('~')
             x1, y1, z1 = map(int, c1.split(','))
             x2, y2, z2 = map(int, c2.split(','))
-            self.bricks.append(Brick(x1, y1, z1, x2, y2, z2))
+            self.bricks.append(Brick(i, x1, y1, z1, x2, y2, z2))
+
+    def fall(self) -> None:
+        ...
--- day22/test.py
+++ day22/test.py
@@ -14,13 +14,33 @@ TEST_DATA = [
 
 
 FALLING_BRICKS = [
-    Brick(1, 0, 1, 1, 2, 1),
-    Brick(0, 0, 2, 2, 0, 2),
-    Brick(0, 2, 3, 2, 2, 3),
-    Brick(0, 0, 4, 0, 2, 4),
-    Brick(2, 0, 5, 2, 2, 5),
-    Brick(0, 1, 6, 2, 1, 6),
-    Brick(1, 1, 8, 1, 1, 9)
+    Brick(0, 1, 0, 1, 1, 2, 1),
+    Brick(1, 0, 0, 2, 2, 0, 2),
+    Brick(2, 0, 2, 3, 2, 2, 3),
+    Brick(3, 0, 0, 4, 0, 2, 4),
+    Brick(4, 2, 0, 5, 2, 2, 5),
+    Brick(5, 0, 1, 6, 2, 1, 6),
+    Brick(6, 1, 1, 8, 1, 1, 9)
+]
+
+SUPPORTING = [
+    [1, 2],
+    [3, 4],
+    [3, 4],
+    [5],
+    [5],
+    [6],
+    []
+]
+
+SUPPORTED_BY = [
+    [],
+    [0],
+    [0],
+    [1, 2],
+    [1, 2],
+    [3, 4],
+    [5],
 ]
 
 
@@ -31,6 +51,12 @@ class TestBricks(unittest.TestCase):
 
     def testParse(self):
         self.assertListEqual(self.bricks.bricks, FALLING_BRICKS)
+
+    def testFall(self):
+        self.bricks.fall()
+        for brick, supporting, supported_by in zip(self.bricks.bricks, SUPPORTING, SUPPORTED_BY):
+            self.assertSetEqual(brick.supporting, {self.bricks.bricks[s] for s in supporting})
+            self.assertSetEqual(brick.supported_by, {self.bricks.bricks[s] for s in supported_by})
 
 
 if __name__ == '__main__':
