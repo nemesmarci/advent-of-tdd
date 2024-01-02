1202bdd98f4aa3951c6dd6b4e617cab8ba08505d
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 15:14:47 +0000

    Day21 reachable - green

--- day21/day21.py
+++ day21/day21.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+from heapq import heappush, heappop
 
 
 class Garden:
@@ -14,5 +15,25 @@ class Garden:
         self.y: int = y
         self.x: int = x
 
+    @staticmethod
+    def around(y, x):
+        return (y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)
+
     def reachable(self, steps) -> int:
-        return 0
+        visited = set()
+        heap = [(0, self.start)]
+        while heap:
+            current_steps, current_plot = heappop(heap)
+            if current_steps > steps or current_plot in visited:
+                continue
+            for neighbour in filter(lambda n: n in self.area, self.around(*current_plot)):
+                heappush(heap, (current_steps + 1, neighbour))
+            if current_steps % 2 == 0:
+                visited.add(current_plot)
+        return len(visited)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as data:
+        garden = Garden(data)
+    print(garden.reachable(64))
