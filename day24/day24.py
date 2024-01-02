cdd82c48a416859e4c06f8dc3ae33aa15fa3396d
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 21:48:34 +0000

    Day24 parse - green

--- day24/day24.py
+++ day24/day24.py
@@ -3,4 +3,7 @@ from typing import Iterable
 
 class Storm:
     def __init__(self, data: Iterable[str]) -> None:
-        self.hails: list[tuple[tuple[int, int, int], tuple[int, int, int]]] = []
+        self.hails: list[tuple[tuple[int, ...], tuple[int, ...]]] = []
+        for line in map(str.strip, data):
+            pos, vel = line.split(' @ ')
+            self.hails.append((tuple(map(int, pos.split(', '))), tuple(map(int, vel.split(', ')))))
