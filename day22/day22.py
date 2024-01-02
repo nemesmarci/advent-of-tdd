1a88f6f4732697ec60ed43660fe2655901717d74
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 16:23:10 +0000

    Day22 parse - green

--- day22/day22.py
+++ day22/day22.py
@@ -8,7 +8,16 @@ class Brick:
         self.supporting: set[Brick] = set()
         self.supported_by: set[Brick] = set()
 
+    def __eq__(self, other) -> bool:
+        return self.x1 == other.x1 and self.y1 == other.y1 and self.z1 == other.z1 and \
+            self.x2 == other.x2 and self.y2 == other.y2 and self.z2 == other.z2
+
 
 class Bricks:
     def __init__(self, data: Iterable[str]) -> None:
         self.bricks: list[Brick] = []
+        for line in data:
+            c1, c2 = line.strip().split('~')
+            x1, y1, z1 = map(int, c1.split(','))
+            x2, y2, z2 = map(int, c2.split(','))
+            self.bricks.append(Brick(x1, y1, z1, x2, y2, z2))
