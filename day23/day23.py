967fdd80cf3bc244b7c8a5ed81dbd84bdca03992
Nemes Marcell <nemes@sch.bme.hu>
2024-01-02 21:24:21 +0000

    Day23 parse - green

--- day23/day23.py
+++ day23/day23.py
@@ -4,5 +4,9 @@ from typing import Iterable
 class Walk:
     def __init__(self, data: Iterable[str]) -> None:
         self.area: dict[tuple[int, int], str] = {}
-        self.y: int = 0
-        self.x: int = 0
+        for y, line in enumerate(map(str.strip, data)):
+            for x, c in enumerate(line):
+                if c != '#':
+                    self.area[(y, x)] = c
+        self.y: int = y
+        self.x: int = x
